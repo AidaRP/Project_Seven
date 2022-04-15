57df3abcf3b3893d9e79c52b06219194c0a31858
AidaRP <90700748+AidaRP@users.noreply.github.com>
2022-04-15 12:54:58 +0200

    feat: added a gamesAll function by GameController

--- backendGames/app/Http/Controllers/GameController.php
+++ backendGames/app/Http/Controllers/GameController.php
@@ -3,8 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Database\QueryException;
+
+use App\Models\Game;
 
 class GameController extends Controller
 {
-    //
+    //All Games
+    public function gamesAll(){
+        try {
+            $games = Game::all();
+            return $games;
+        } catch (QueryException $error) {
+            $codeError = $error->errorInfo[1];
+            if ($codeError) {
+                return "Error $codeError";
+            }
+        }
+    }
+
+    //Game by ID
+    public function gameById (Request $request){
+        
+    }
+
+
+
 }
