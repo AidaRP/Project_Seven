326ce710490487776b8f2e90af0354514fb5a383
AidaRP <90700748+AidaRP@users.noreply.github.com>
2022-04-15 20:51:46 +0200

    feat: added gameById function by GameController

--- backendGames/app/Http/Controllers/GameController.php
+++ backendGames/app/Http/Controllers/GameController.php
@@ -58,18 +58,19 @@ class GameController extends Controller
             }
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
+//Game By Id 
+public function gameById(Request $request){
+    $id = $request->input('id');
+    try {
+        $game = Game::all()->where('id', "=", $id);
+        return $game;
+    } catch (QueryException $error) {
+        $codeError = $error->errorInfo[1];
+        if($codeError){
+            return"Error $codeError";
+        }
+    }
+}
 
 
 
