47ce366aed0939b76dfd7970ee829759cbe2d600
AidaRP <90700748+AidaRP@users.noreply.github.com>
2022-04-15 13:15:10 +0200

    feat: added a gameAdd function by GameController

--- backendGames/app/Http/Controllers/GameController.php
+++ backendGames/app/Http/Controllers/GameController.php
@@ -10,7 +10,8 @@ use App\Models\Game;
 class GameController extends Controller
 {
     //All Games
-    public function gamesAll(){
+    public function gamesAll()
+    {
         try {
             $games = Game::all();
             return $games;
@@ -23,12 +24,32 @@ class GameController extends Controller
     }
 
     //Game by ID
-    public function gameById (Request $request){
+    public function gameById(Request $request)
+    {
         $id = $request->input('id');
 
         try {
             $game = Game::all()->where('id', "=", $id);
             return $game;
+        } catch (QueryException $error) {
+            $codeError = $error->errorInfo[1];
+            if ($codeError) {
+                return "Error $codeError";
+            }
+        }
+    }
+
+    //Create Game
+    public function gameAdd (Request $request){
+        $title = $request->input('title');
+        $splashArtUrl = $request->input('splashArtUrl');
+        $url = $request->input('url');
+        try {
+            return Game::create([
+                'title'=>$title,
+                'splashArtUrl'=>$splashArtUrl,
+                'url'=>$url
+            ]);
         } catch (QueryException $error) {
             $codeError = $error->errorInfo[1];
             if($codeError){
@@ -38,6 +59,21 @@ class GameController extends Controller
         }
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
-
-
