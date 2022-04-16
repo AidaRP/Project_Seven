f434aa20fba624651073c08653a8104c44bcfd6b
AidaRP <90700748+AidaRP@users.noreply.github.com>
2022-04-16 11:03:15 +0200

    feat: added a message fucntion by MessageController

--- backendGames/app/Http/Controllers/MessageController.php
+++ backendGames/app/Http/Controllers/MessageController.php
@@ -1,10 +1,27 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Database\QueryException;
+use App\Models\Message;
 
 use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
-    //
+    //All messages
+    public function messagesAll(){
+        try {
+
+            $messages = Message::all();
+            return $messages;
+
+        } catch (QueryException $error) {
+
+            $codeError = $error->errorInfo[1];
+            if($codeError){
+                return "Error $codeError";
+            }
+
+        }
+    }
 }
