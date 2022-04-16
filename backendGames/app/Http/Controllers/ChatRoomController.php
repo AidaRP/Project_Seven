6cd9731f676c06e28117fb0c7c56484053c75db7
AidaRP <90700748+AidaRP@users.noreply.github.com>
2022-04-16 23:07:09 +0200

    feat: added a chatAll function by ChatRoomController

--- backendGames/app/Http/Controllers/ChatRoomController.php
+++ backendGames/app/Http/Controllers/ChatRoomController.php
@@ -3,8 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Database\QueryException;
+
+use App\Models\ChatRoom;
 
 class ChatRoomController extends Controller
 {
-    //
+    //All chats
+    public function chatAll(){
+        try {
+
+            $chats = ChatRoom::all();
+            return $chats;
+
+        } catch (QueryException $error) {
+
+            $codeError = $error->errorInfo[1];
+            if($codeError){
+                return "Error $codeError";
+            }
+
+        }
+    }
+//Chats by Id
+
+
+
+
+
+
 }
