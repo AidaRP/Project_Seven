c634b81957e69cf65869a8385cb9f8135204e47c
AidaRP <90700748+AidaRP@users.noreply.github.com>
2022-04-17 23:45:23 +0200

    feat: refactorizing PLayerController

--- backendGames/app/Http/Controllers/PlayerController.php
+++ backendGames/app/Http/Controllers/PlayerController.php
@@ -5,98 +5,113 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Database\QueryException;
 
-use App\Models\Player;
+use App\Models\User;
 
 class PlayerController extends Controller
 {
     //List All Players
-    public function playersAll()
-    {
-
+    public function playersAll(){
         try {
-            $players = Player::all();
+
+            $players = User::all();
             return $players;
+
         } catch (QueryException $error) {
 
-            $codeError = $error->errorInfo[1];
-            if ($codeError) {
-                return "Error $codeError";
+            $codigoError = $error->errorInfo[1];
+            if($codigoError){
+                return "Error $codigoError";
             }
+
         }
     }
 
-    //List Player By Id
-    public function playerById(Request $request)
-    {
-
+    //Player/User by Id
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public function playerByID(Request $request){
+        
         $id = $request->input('id');
+
         try {
-            $player = Player::all()->where('id', "=", $id);
+            $player = User::all()
+            ->where('id', "=", $id);
             return $player;
+
         } catch (QueryException $error) {
-            $codeError = $error->errorInfo[1];
-            if ($codeError) {
-                return "Error $codeError";
+
+            $codigoError = $error->errorInfo[1];
+            if($codigoError){
+                return "Error $codigoError";
             }
         }
     }
 
-    //Update Player
-    public function updatePlayer(Request $request)
-    {
+    //Update User/Player
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public function playerUpdate (Request $request){
 
         $id = $request->input('id');
-        $name = $request->input('name');
-        $nick = $request->input('nick');
+        $username = $request->input('username');
         $email = $request->input('email');
         $password = $request->input('password');
         $role = $request->input('role');
 
         try {
-            $player = Player::where('id', "=", $id)->update(
+
+            $player = User::where('id', '=', $id)
+            ->update(
                 [
-                    'name' => $name,
-                    'nick' => $nick,
+                    'username' => $username,
                     'email' => $email,
                     'password' => $password,
                     'role' => $role,
                 ]
             );
-            return Player::all()->where('id', "=", $id);
+            return User::all()
+            ->where('id', "=", $id);
+
         } catch (QueryException $error) {
-            $codeError = $error->errorInfo[1];
-            if ($codeError) {
-                return "Error $codeError";
+
+            $codigoError = $error->errorInfo[1];
+            if($codigoError){
+                return "Error $codigoError";
             }
+
         }
     }
-    //Delete Player by Id
-    public function deletePlayerById(Request $request){
+
+    //Delete user by Id
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public function playerDelete(Request $request){
+
         $id = $request->input('id');
 
         try {
-            $arrayPlayer = Player::all()->where('id', "=", $id);
+            $arrayPlayer = User::all()
+            ->where('id', '=', $id);
 
-            $player = Player::where('id', '=', $id);
-
-            if(count($arrayPlayer) == 0){
+            $player = User::where('id', '=', $id);
+            
+            if (count($arrayPlayer) == 0) {
                 return response()->json([
-                    "data"=> $arrayPlayer,
-                    "message"=> "Player not found"
+                    "data" => $arrayPlayer,
+                    "message" => "No se ha encontrado el player"
                 ]);
             }else{
                 $player->delete();
                 return response()->json([
-                    "data"=> $arrayPlayer,
-                    "message"=> "Player has been deleted succesfully"
+                    "data" => $arrayPlayer,
+                    "message" => "Player borrado correctamente"
                 ]);
             }
+
         } catch (QueryException $error) {
-            $codeError = $error->errorInfo[1];
-            if($codeError){
-                return "Error $codeError";
+
+            $codigoError = $error->errorInfo[1];
+            if($codigoError){
+                return "Error $codigoError";
             }
+
         }
     }
-
 }
