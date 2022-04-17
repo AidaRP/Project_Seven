65b79e6930b6a9a5a35a0108b1b5bf77b65a3b92
AidaRP <90700748+AidaRP@users.noreply.github.com>
2022-04-18 00:02:14 +0200

    feat: added a membersAll function

--- backendGames/app/Http/Controllers/MemberController.php
+++ backendGames/app/Http/Controllers/MemberController.php
@@ -1,10 +1,26 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Database\QueryException;
+use App\Models\Member;
 
 use Illuminate\Http\Request;
 
 class MemberController extends Controller
 {
-    //
+    //List all Members
+    public function membersAll(){
+        try {
+
+            $members = Member::all();
+            return $members;
+
+        } catch (QueryException $error) {
+
+            $codigoError = $error->errorInfo[1];
+            if($codigoError){
+                return "Error $codigoError";
+            }
+
+        }
 }
